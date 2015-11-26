6638e98f4afa15e14cdf9e474758d2cf7b2181a9
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2015-11-26 11:25:33 +0100

    final version

--- deploy.php
+++ deploy.php
@@ -2,8 +2,10 @@
 
 require 'recipe/common.php';
 
+set('user', 'tymdad');
+
 server('prod', 'www183.your-server.de', 222)
-    ->user('tymdad')
+    ->user(get('user'))
     ->forwardAgent()
     ->stage('production')
     ->env('deploy_path', '/usr/www/users/tymdad/doebbelin.net');
@@ -11,21 +13,24 @@ server('prod', 'www183.your-server.de', 222)
 set('repository', 'git@github.com:fdoebbelin/doebbelin_blog.git');
 set('keep_releases', 3);
 
-set('shared_dirs', [
-    'storage/app',
-    'storage/framework/cache',
-    'storage/framework/sessions',
-    'storage/framework/views',
-    'storage/logs',
-]);
+set('shared_dirs', ['storage', 'vendor']);
 
 set('shared_files', ['.env']);
+
+task('deploy:rsync_vendors', function () {
+    $rsync_cmd =
+        'rsync -avz --delete -e "ssh -p ' . env('server.port') . '" ' .
+        getcwd() . '/vendor ' .
+        get('user') . '@' . env('server.host') . ':' .
+        env('deploy_path') . '/shared';
+    runLocally($rsync_cmd);
+});
 
 task('deploy', [
     'deploy:prepare',
     'deploy:release',
     'deploy:update_code',
-    //'deploy:vendors',
+    'deploy:rsync_vendors',
     'deploy:shared',
     'deploy:symlink',
     'cleanup'
