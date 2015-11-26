e6764101126d0e89645b2dec05ec1cf1e59abce4
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2015-11-26 16:57:31 +0100

    version 22

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">doebbelin.net</div>
-                <p>Version 21</p>
+                <p>Version 22</p>
             </div>
         </div>
     </body>
