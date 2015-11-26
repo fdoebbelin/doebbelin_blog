8d3febfc6f61bf2fbe61b626adad621597d37b31
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2015-11-26 10:29:00 +0100

    with shared

--- deploy.php
+++ deploy.php
@@ -10,3 +10,25 @@ server('prod', 'www183.your-server.de', 222)
 
 set('repository', 'git@github.com:fdoebbelin/doebbelin_blog.git');
 set('keep_releases', 3);
+
+set('shared_dirs', [
+    'storage/app',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+]);
+
+set('shared_files', ['.env']);
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    //'deploy:vendors',
+    'deploy:shared',
+    'deploy:symlink',
+    'cleanup'
+])->desc('Deploy your project');
+
+after('deploy', 'success');
