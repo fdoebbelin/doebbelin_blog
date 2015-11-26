0c6ba698f4e33853a2c6ae19097066a877cd394e
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2015-11-26 09:43:45 +0100

    new deployment with deployer

---
+++ deploy.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'recipe/composer.php';
+
+server('prod', 'www183.your-server.de', 222)
+    ->user('tymdad')
+    ->forwardAgent()
+    ->stage('production')
+    ->env('deploy_path', '/usr/www/users/tymdad/doebbelin.net');
+
+set('repository', 'git@github.com:fdoebbelin/doebbelin_blog.git');
+set('keep_releases', 3);
