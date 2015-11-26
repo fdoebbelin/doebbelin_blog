b2074e5d8ec88cbe242b818e88b156bba11d7948
Fritz-Rainer Doebbelin <frd@doebbelin.net>
2015-11-26 13:10:51 +0100

    neuer Version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Doebbelin.net</div>
+                <div class="title">doebbelin.net</div>
             </div>
         </div>
     </body>
